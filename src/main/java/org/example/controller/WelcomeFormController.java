566ad4e8c9d9a36e8af32f11854dc5fd6fdb10b5
chamika-damith <chamikadamith9@gmail.com>
2024-01-15 01:32:34 +0530

    Create Welcome form

---
+++ src/main/java/org/example/controller/WelcomeFormController.java
@@ -0,0 +1,9 @@
+package org.example.controller;
+
+import javafx.event.ActionEvent;
+
+public class WelcomeFormController {
+    public void btnGetStartOnAction(ActionEvent actionEvent) {
+
+    }
+}
